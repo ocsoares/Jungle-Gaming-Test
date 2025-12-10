5616763de6b1e9c9639f2c1b37401de4cd915509
Cauã Soares <casoares016@gmail.com>
2025-12-10 01:29:49 -0300

    chore: update configs files

--- packages/config/src/server.config.ts
+++ packages/config/src/server.config.ts
@@ -1,1 +1,25 @@
-export const todo = "todo";
+import { registerAs } from "@nestjs/config";
+
+export interface IServerConfig {
+    postgres: {
+        host?: string;
+        port?: number;
+        username?: string;
+        password?: string;
+        database?: string;
+    };
+}
+
+export default registerAs<IServerConfig>("server", () => {
+    return {
+        postgres: {
+            host: process.env.POSTGRES_HOST || "localhost",
+            port: process.env.POSTGRES_PORT
+                ? parseInt(process.env.POSTGRES_PORT, 10)
+                : 5432,
+            username: process.env.POSTGRES_USER || "postgres",
+            password: process.env.POSTGRES_PASSWORD || "postgres",
+            database: process.env.POSTGRES_DB || "postgres",
+        },
+    };
+});
