1f0199382c52c81f22b55e50f5dcd4915d024e2a
Cauã Soares <casoares016@gmail.com>
2025-12-09 23:11:31 -0300

    chore: add database package & user entity

---
+++ packages/database/typeorm/src/entities/user.entity.ts
@@ -0,0 +1,28 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from "typeorm";
+
+@Entity({ name: "users" })
+export class UserEntity {
+    @PrimaryGeneratedColumn("uuid")
+    readonly id: string;
+
+    @Column({ unique: true })
+    readonly username: string;
+
+    @Column({ unique: true })
+    readonly email: string;
+
+    @Column({ select: false, length: 256 })
+    readonly password: string;
+
+    @CreateDateColumn()
+    readonly createdAt: Date;
+
+    @UpdateDateColumn()
+    readonly updatedAt: Date;
+}
